c6ba889887db142b211aaf94830e6c2dbd6fb620
Vertash Valery <35772829+vvertash@users.noreply.github.com>
2020-12-06 19:12:53 +0300

    Components data handler added

--- app/src/main/java/ru/innohelpers/innohelp/services/delivery/IDeliveryProvider.kt
+++
@@ -1,7 +0,0 @@
-package ru.innohelpers.innohelp.services.delivery
-
-import ru.innohelpers.innohelp.data.delivery.Delivery
-
-interface IDeliveryProvider {
-    suspend fun getAll(): Collection<Delivery>
-}
